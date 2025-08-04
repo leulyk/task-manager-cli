9783b2f35d351dfeed5f1defb4990460e8e3237b
Leul Yiheyis <leulykpro@gmail.com>
2025-08-04 23:29:44 +0300

    feat: modify arguments for constructors and add the clone trait

--- src/models.rs
+++ src/models.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
+#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 pub struct Epic {
     pub name: String,
     pub description: String,
@@ -10,17 +10,17 @@ pub struct Epic {
 }
 
 impl Epic {
-    pub fn new(name: &str, description: &str) -> Self {
+    pub fn new(name: String, description: String) -> Self {
         Self {
-            name: name.to_owned(),
-            description: description.to_owned(),
+            name,
+            description,
             status: Status::Open,
             stories: vec![],
         }
     }
 }
 
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
+#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 pub enum Status {
     Open,
     InProgress,
@@ -28,7 +28,7 @@ pub enum Status {
     Closed,
 }
 
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
+#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 pub struct Story {
     pub name: String,
     pub description: String,
@@ -36,16 +36,16 @@ pub struct Story {
 }
 
 impl Story {
-    pub fn new(name: &str, description: &str) -> Self {
+    pub fn new(name: String, description: String) -> Self {
         Self {
-            name: name.to_owned(),
-            description: description.to_owned(),
+            name,
+            description,
             status: Status::Open,
         }
     }
 }
 
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
+#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 pub struct DBState {
     pub last_item_id: u32,
     pub epics: HashMap<u32, Epic>,

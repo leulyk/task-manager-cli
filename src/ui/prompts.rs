f12acf0c6275101d86430f5f496bff53e0f056d6
Leul Yiheyis <leulykpro@gmail.com>
2025-08-12 16:16:26 +0300

    feat: implement prompts

--- src/ui/prompts.rs
+++ src/ui/prompts.rs
@@ -24,21 +24,51 @@ impl Prompts {
 }
 
 fn create_epic_prompt() -> Epic {
-    todo!();
+    println!("----------------------------");
+    println!("Epic Name: ");
+    let name = get_user_input();
+    println!("Epic Description: ");
+    let description = get_user_input();
+
+    Epic::new(name, description)
 }
 
 fn create_story_prompt() -> Story {
-    todo!();
+    println!("----------------------------");
+    println!("Story Name: ");
+    let name = get_user_input();
+    println!("Story Description: ");
+    let description = get_user_input();
+
+    Story::new(name, description)
 }
 
 fn delete_epic_prompt() -> bool {
-    todo!();
+    println!("----------------------------");
+    println!("Are you sure you want to delete this epic? All stories in this epic will also be deleted [Y/n]: ");
+    let response = get_user_input();
+
+    response.trim() == "Y"
 }
 
 fn delete_story_prompt() -> bool {
-    todo!();
+    println!("----------------------------");
+    println!("Are you sure you want to delete this story? [Y/n]: ");
+
+    get_user_input().trim() == "Y"
 }
 
 fn update_status_prompt() -> Option<Status> {
-    todo!();
+    println!("----------------------------");
+    println!("New Status (1 - OPEN, 2 - IN-PROGRESS, 3 - RESOLVED, 4 - CLOSED): ");
+
+    let response = get_user_input().trim().parse::<u32>().ok()?;
+
+    match response {
+        1 => Some(Status::Open),
+        2 => Some(Status::InProgress),
+        3 => Some(Status::Resolved),
+        4 => Some(Status::Closed),
+        _ => None,
+    }
 }
